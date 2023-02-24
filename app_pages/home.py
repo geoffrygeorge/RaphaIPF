448eb88df04c79049fd5290a6b4d12ac6016bd78
Geoffry George Mathew <79758301+geoffrygeorge@users.noreply.github.com>
2023-02-24 13:10:54 +0300

    added container in home.py

--- app_pages/home.py
+++ app_pages/home.py
@@ -23,79 +23,80 @@ def home_page():
 
     home_bg("Assets/images/PRAY.jpg")
 
-    ## RAPHA LOGO
-    # 1st method deprecated
-    if False:
-        def img_to_bytes(LOGO_PATH):
-            IMG_BYTES = Path(LOGO_PATH).read_bytes()
-            ENCODED = base64.b64encode(IMG_BYTES).decode()
-            return ENCODED
+    with st.container():
+        
+        ## RAPHA LOGO
+        # 1st method deprecated
+        if False:
+            def img_to_bytes(LOGO_PATH):
+                IMG_BYTES = Path(LOGO_PATH).read_bytes()
+                ENCODED = base64.b64encode(IMG_BYTES).decode()
+                return ENCODED
 
-        def img_to_html(LOGO_PATH):
-            IMG_HTML = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(img_to_bytes(LOGO_PATH))
-            return IMG_HTML
+            def img_to_html(LOGO_PATH):
+                IMG_HTML = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(img_to_bytes(LOGO_PATH))
+                return IMG_HTML
 
-        st.markdown(
-            "<p style = 'text-align: center;'>"+img_to_html('Assets/images/RAPHA LOGO.png')+"</p>", unsafe_allow_html = True
-            )
+            st.markdown(
+                "<p style = 'text-align: center;'>"+img_to_html('Assets/images/RAPHA LOGO.png')+"</p>", unsafe_allow_html = True
+                )
 
-    # 2nd updated method
-    def home_logo(HOME_RAPHA_LOGO):
-        with open(HOME_RAPHA_LOGO, "rb") as HOME_RAPHA_LOGO:
-            ENCODED_STRING = base64.b64encode(HOME_RAPHA_LOGO.read())
+        # 2nd updated method
+        def home_logo(HOME_RAPHA_LOGO):
+            with open(HOME_RAPHA_LOGO, "rb") as HOME_RAPHA_LOGO:
+                ENCODED_STRING = base64.b64encode(HOME_RAPHA_LOGO.read())
+            st.markdown(
+                """
+                <style>
+                img {
+                    display: block;
+                    max-width: 100%;
+                    max-height: 100%;
+                    margin-left: auto;
+                    margin-right: auto;
+                    padding-bottom: 50px;
+                }
+
+                .zoom {
+                    transition: transform .2s;
+                }
+
+                .zoom:hover {
+                    transform: scale(1.05);
+                }
+                </style>
+                """, unsafe_allow_html = True)
+            
+            st.markdown(
+                f"""
+                <img src = "data:image/{"png"};base64,{ENCODED_STRING.decode()}" class="zoom">""", unsafe_allow_html = True)
+
+        home_logo("Assets/images/RAPHA LOGO.png")
+
+        ## MAIN TITLE
+        # <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'/>
+        # https://www.justinmind.com/blog/best-google-web-fonts-website/
         st.markdown(
             """
             <style>
-            img {
-                display: block;
-                max-width: 100%;
-                max-height: 100%;
-                margin-left: auto;
-                margin-right: auto;
-                margin-top: -30px;
-            }
-
-            .zoom {
-                transition: transform .2s;
-            }
-
-            .zoom:hover {
-                transform: scale(1.05);
-            }
+                @import url(https://fonts.googleapis.com/css?family=Roboto);
+                .h1 {
+                    text-align: center;
+                    font-size: calc(1.0em + 4.5vmin);
+                    font-family: 'Roboto', sans-serif;
+                    font-weight: bold;
+                    color: white;
+                }
             </style>
             """, unsafe_allow_html = True)
-        
+
+        st.markdown("<p class='h1'>RAPHA INTERNATIONAL MINISTRIES</p>", unsafe_allow_html = True)
+
+        st.markdown("<h2 style='text-align: center; color: white;'>SINCE 2020</h2>", unsafe_allow_html = True)
+
         st.markdown(
-            f"""
-            <img src = "data:image/{"png"};base64,{ENCODED_STRING.decode()}" class="zoom">""", unsafe_allow_html = True)
-
-    home_logo("Assets/images/RAPHA LOGO.png")
-
-    ## MAIN TITLE
-    # <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'/>
-    # https://www.justinmind.com/blog/best-google-web-fonts-website/
-    st.markdown(
-        """
-        <style>
-            @import url(https://fonts.googleapis.com/css?family=Roboto);
-            .h1 {
-                text-align: center;
-                padding-top: 10px;
-                font-size: calc(1.0em + 4.5vmin);
-                font-family: 'Roboto', sans-serif;
-                font-weight: bold;
-                color: white;
-            }
-        </style>
-        """, unsafe_allow_html = True)
-
-    st.markdown("<p class='h1'>RAPHA INTERNATIONAL MINISTRIES</p>", unsafe_allow_html = True)
-
-    st.markdown("<h2 style='text-align: center; color: white;'>SINCE 2020</h2>", unsafe_allow_html = True)
-
-    st.markdown(
-        """
-        <style>
-        .css-eczf16 {display: none}
-        </style>
-        """, unsafe_allow_html = True)
+            """
+            <style>
+            .css-eczf16 {display: none}
+            </style>
+            """, unsafe_allow_html = True)
