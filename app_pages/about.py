d2784fccff0bffcc0c3c323a6bf2d2e9d9fbe873
Geoffry George Mathew <jeffrygeorge58@gmail.com>
2023-02-25 12:52:14 +0300

    added first title in about us

--- app_pages/about.py
+++ app_pages/about.py
@@ -23,3 +23,37 @@ def about_page():
             """, unsafe_allow_html = True)
 
     about_bg('Assets/images/SHINY.svg')
+
+    with st.container():
+
+        st.markdown("""
+            <style>
+                @import url(https://fonts.googleapis.com/css?family=Roboto);
+                .h2 {
+                    text-align: center;
+                    margin-top:-50px;
+                    font-size: calc(1.0em + 4vmin);
+                    font-family: 'Roboto', sans-serif;
+                    font-weight: bold;
+                    color: white;
+                }
+            </style>
+        """, unsafe_allow_html=True)
+
+        st.markdown('<p class="h2">Our Ministry</p>', unsafe_allow_html = True)
+
+        st.markdown("""
+            <style>
+                @import url(https://fonts.googleapis.com/css?family=Roboto);
+                .medium-font {
+                    text-align: justify;
+                    margin-bottom:70px;
+                    font-size: calc(1.0em + 1vmin);
+                    font-family: 'Roboto', sans-serif;
+                    font-weight: medium;
+                    color: white;
+                }
+            </style>
+        """, unsafe_allow_html=True)
+
+        st.markdown("""<p class="medium-font">Our ministry is called <strong>'Rapha International Prayer Fellowship'</strong>. 'Rapha' means healer because our God is the healer. We praise our Lord God and His name is <strong>Jesus</strong>. He had no servants, yet they called Him <strong>Master</strong>. He had no worldly educational qualifications, yet they called <strong>Teacher</strong>. He had no medicines, yet they called him <strong>Healer</strong>. He had no army, yet Kings feared Him. He won no military battles, yet He conquered the world through His Love. He committed no crime, yet they crucified Him. He was then buried in a tomb, and yet <strong>HE LIVES FOREVER AND EVER</strong>.</p>""", unsafe_allow_html = True)
