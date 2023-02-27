685ba3fafd37215be38cce5a98e8f4cf5029c8ca
Geoffry George Mathew <79758301+geoffrygeorge@users.noreply.github.com>
2023-02-27 22:00:16 +0300

    reverted to the og. sidebar UI

--- rapha_main.py
+++ rapha_main.py
@@ -59,12 +59,7 @@ with st.sidebar:
         icons = ["house", "info-circle"], 
         menu_icon = "menu-up", 
         default_index = 0, 
-        orientation = "vertical",
-        styles={"container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    })
+        orientation = "vertical")
 
 # ----- HOME PAGE -----
 if MAIN_MENU == "Home":
