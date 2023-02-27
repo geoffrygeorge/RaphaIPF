34071a696fa2876dc7de8c3c7769a877a18bb4a7
Geoffry George Mathew <79758301+geoffrygeorge@users.noreply.github.com>
2023-02-27 21:57:02 +0300

    changed sidebar options UI

--- rapha_main.py
+++ rapha_main.py
@@ -59,7 +59,12 @@ with st.sidebar:
         icons = ["house", "info-circle"], 
         menu_icon = "menu-up", 
         default_index = 0, 
-        orientation = "vertical")
+        orientation = "vertical",
+        styles={"container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    })
 
 # ----- HOME PAGE -----
 if MAIN_MENU == "Home":
