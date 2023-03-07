33bad8f666161c53d4c5b4abffd08b2068534fe7
Geoffry George Mathew <jeffrygeorge58@gmail.com>
2023-03-07 10:18:19 +0300

    added separate css styles for pages

--- pages/02_Meetings.py
+++ pages/02_Meetings.py
@@ -20,7 +20,7 @@ def app_style(file_name):
     with open(file_name) as f:
         st.markdown(f"""<style>{f.read()}</style>""", unsafe_allow_html=True)
 
-app_style("styles/about_style.css")
+app_style("styles/meetings_style.css")
 
 
 # ----- MEETINGS BACKGROUND -----
