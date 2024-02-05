37c2eb1a12b7dfddd6c6669064733c45a2913b82
Geoffry George Mathew <79758301+geoffrygeorge@users.noreply.github.com>
2024-02-05 21:08:20 +0300

    added corrected faith statement

--- src/statements/faith.py
+++ src/statements/faith.py
@@ -1,5 +1,5 @@
 """
-OUR MINISTRY SECTION
+FAITH STATEMENT SECTION
 """
 
 # ----- IMPORTING NECESSARY LIBRARIES
@@ -10,7 +10,7 @@ def faith_main():
     st.markdown("""
         <style>
             @import url(https://fonts.googleapis.com/css?family=Montserrat);
-            .ministry {
+            .faith {
                 text-align: center;
                 margin-top:-50px;
                 font-size: calc(1.0em + 4vmin);
@@ -21,13 +21,13 @@ def faith_main():
         </style>
     """, unsafe_allow_html = True)
 
-    st.markdown("<h2 class = 'ministry'>FAITH STATEMENT</h2>", unsafe_allow_html = True)
+    st.markdown("<h2 class = 'faith'>FAITH STATEMENT</h2>", unsafe_allow_html = True)
 
     # DESCRIPTION
     st.markdown("""
         <style>
             @import url(https://fonts.googleapis.com/css?family=Montserrat);
-            .ministry_medium-font {
+            .faith_medium-font {
                 text-align: justify;
                 font-size: calc(1.0em + 0.5vmin);
                 font-family: 'Montserrat', sans-serif;
@@ -37,4 +37,4 @@ def faith_main():
         </style>
     """, unsafe_allow_html = True)
 
-    st.markdown("""<p class = "ministry_medium-font">Rapha International Ministries (RIM)We holds the Bible-based believe system. Triune God-theFather,the Son and the Holy Spirit, is the Creator of everything, the father of all true believers,transcendent and immanent. He alone, is worthy to be worshiped. The Bible that contains 66 booksalone is the inspired word of God, unchangeable, everlasting, infallible and the final court for everyissue. It holds that Jesus is the revealed God in the flesh and accepts His redemptive work on thecross of Calvary for all world, and the redeemed community the Church. He is the final judge of theworld. It also holds that God the Spirit is preparing the universal church of God at present and thebelief on the everlasting destiny of man, heaven or hell.</p>""", unsafe_allow_html = True)
+    st.markdown("""<p class = "faith_medium-font">Rapha International Ministries (RIM). We hold the Bible-based belief system. Triune God - the Father, the Son, and the Holy Spirit, is the Creator of everything, the father of all true believers, transcendent and immanent. He alone is worthy to be worshiped. The Bible which contains 66 books alone is the inspired word of God, unchangeable, everlasting, infallible, and the final court for every issue. It holds that Jesus is the revealed God in the flesh and accepts His redemptive work on thecross of Calvary for all the world, and the redeemed community of the Church. He is the final judge of the world. It also holds that God the Spirit is preparing the universal church of God at present and the belief in the everlasting destiny of man, heaven or hell.</p>""", unsafe_allow_html = True)
